8e9d415109163ab9bd7876fd9bf933ef1deb2573
KEMMLER Lucas <kemmler2u@etu.univ-lorraine.fr>
2024-06-18 08:12:37 +0200

    Adding base classes

--- Voiture.py
+++ Voiture.py
@@ -8,51 +8,30 @@ class Voiture :
         # la liste des etats possibles
         etats = []
         for pos in range(TAILLE) :
-            etats.append((pos,True))
-            etats.append((pos,False))
+            etats.append((pos,"haut"))
+            etats.append((pos,"bas"))
         return etats
 
     def actions(self):
         # retourne la liste des actions
-        actions = ["haut" ,"bas"]
+        actions = ["haut","bas"]
         return actions
 
     def etatSuivant(self, etat, action):
         # raisonner par action
-        (pos,cafe) = etat
+        (pos,direction) = etat
 
-        if action == "gauche" :
-            pos = pos - 1
-            if pos < 0 :
+        if action == "haut" :
+            pos = pos + 1
+            if pos < 0 and direction =="haut" :
                 pos = 0
 
-        if action == "droite" :
-            pos = pos + 1
-            if pos > TAILLE - 1 :
+        if action == "bas" :
+            pos = pos - 1
+            if pos > TAILLE - 1 and direction =="bas":
                 pos = TAILLE - 1
+        return (pos,direction)
 
-        if action == "poser" :
-            cafe = False
-
-        if action == "prendre":
-            if pos == 0:
-                cafe = True
-
-        return (pos,cafe)
-
-
-    def recompense(self, etat, action, etatF):
-        # recompenses spéciales
-        (pos,cafe) = etat
-        # si au debut pose et a le cafe
-        if (pos == TAILLE-1) and (cafe == True) and (action == "poser") :
-            return 10
-
-        # sinon retourne -1
-        if (action == "rien") :
-            return 0
-
-        return -1
 
 
 
